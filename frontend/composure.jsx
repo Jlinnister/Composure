80361cf6e64274a5425fb156336879ab44e94059
Jeff Lin <jlin28@wustl.edu>
2016-08-29 00:55:39 -0700

    basic splash

--- frontend/composure.jsx
+++ frontend/composure.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const composure = (
+    <div className="Aligner-item">COMPOSURE</div>
+);
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render((
-    <div>hello</div>), document.getElementById('root'));
+  ReactDOM.render(composure, document.getElementById('root'));
 });
