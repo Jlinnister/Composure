ae165c8c15101e2214286acc85f30d654faecba4
Jeff Lin <jlin28@wustl.edu>
2016-08-28 19:00:55 -0700

    created database, added static route, controller and view, and main react component hook

---
+++ frontend/composure.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render((
+    <div>hello</div>), document.getElementById('root'));
+});
