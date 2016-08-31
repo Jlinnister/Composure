3d1a66cc977ea0be8eedef47fbefa1020d44c1ea
Jeff Lin <jlin28@wustl.edu>
2016-08-31 09:06:04 -0700

    text areas json response in correct format and updates store

--- app/views/api/text_areas/_text_area.json.jbuilder
+++ app/views/api/text_areas/_text_area.json.jbuilder
@@ -1,3 +1,3 @@
-# json.extract! @text_area, :id, :title, :body, :position, :story_id
+# json.extract! @text_areas, :id, :title, :body, :position, :story_id
 
 json.array! @text_areas, :id, :title, :body, :position, :story_id
--- frontend/reducers/text_area_reducer.js
+++ frontend/reducers/text_area_reducer.js
@@ -4,8 +4,9 @@ import { TextAreaConstants } from '../actions/text_area_actions';
 const TextAreaReducer = (state = {}, action) => {
   switch (action.type) {
     case TextAreaConstants.RECEIVE_TEXT_AREA:
-      const newTextArea = { [action.text_area.id]: action.text_area };
-      return Object.assign({}, state, newTextArea);
+      const textAreas = {}
+      action.text_area.forEach( item => textAreas[item.id] = item )
+      return Object.assign({}, state, textAreas);
     case TextAreaConstants.DESTROY_TEXT_AREA:
       let newState = Object.assign({}, state);
       delete newState[action.text_area.id];
